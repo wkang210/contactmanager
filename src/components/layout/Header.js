1e7b52d79f672ddc0511fcfa0463302fabe64388
Won Kang <wonyoungkang@gmail.com>
2019-08-24 16:20:29 -0400

    minor css fix

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -8,7 +8,7 @@ const Header = (props) => {
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
             <div className="container">
-                <a href="/" className="navbar-brand">{branding}</a>
+                <a href="/" className="navbar-brand" style={{display:'contents'}}>{branding}</a>
                 <div>
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item">
